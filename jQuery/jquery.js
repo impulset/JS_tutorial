e72d5b8d86a6e90285e74eae615af9d5f7a8e302
Pavel Sizykh <sizykh86@gmail.com>
2022-02-17 22:53:14 -0500

    added jquery

---
+++ jQuery/jquery.js
@@ -0,0 +1,18 @@
+$("li").click(function(ev){
+    console.log(ev.target.innerText)
+})
+
+// get text
+$("li").text()
+
+// set text
+$("li").text("some text")
+
+// get html
+$("li").html()
+
+// set attribute
+$("li").attr("name", "new_name")
+
+$("li").attr({name: "other_name",
+             class: "new_class"})
